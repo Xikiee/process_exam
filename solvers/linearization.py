1ebb36496fe1216e57821ba218cae205cb47df37
X1kiee <oliverl.lohr@gmail.com>
2025-04-08 18:24:31 +0200

    sssssseeeeeyyyyyyaaaaahhhhh

--- solvers/linearization.py
+++ solvers/linearization.py
@@ -4,24 +4,16 @@ import control as ctrl
 import sympy as sp
 
 #### linearization of a system
-x = sp.symbols('x', real= True)
-eq = 4*x**0.5
-x0 = 1
+def  linearization_of_system(eq):
+    x = sp.Symbol('x')
+    xs = sp.Symbol('x_s') 
 
+    F_taylor = eq.subs(x, xs) + sp.diff(eq, x).subs(x, xs) * (x - xs)
 
+    return sp.pprint(F_taylor)
 
-import sympy as sp
-def  linearization_of_system(eq,x0):
-
-# Define the symbol
-x = sp.Symbol('x')
-xs = sp.Symbol('x_s')  # Operating point
-
-# Define the function
-F = 4 * sp.sqrt(x)
-
-# First order Taylor series expansion around x_s
-F_taylor = F.subs(x, xs) + sp.diff(F, x).subs(x, xs) * (x - xs)
-
-# Display the linearized form
-sp.pprint(F_taylor)
+#### how to use
+# x = sp.Symbol('x')
+# xs = sp.Symbol('x_s')  # Operating point
+# eq = 4 * sp.sqrt(x)
+# linearization_of_system(eq)
