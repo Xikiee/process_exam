51babc840a6ea51b569492f06f4c92b391528f17
X1kiee <oliverl.lohr@gmail.com>
2025-04-08 19:04:13 +0200

    linearization changes

--- solvers/linearization.py
+++ solvers/linearization.py
@@ -20,9 +20,8 @@ def  linearization_of_system(eq):
 
 #### linearization of a system with multiple variables
 
-def linearization_system_with_multiple_variables(eq):
+def linearization_system_with_multiple_variables(eq, X_o, Y_o):
     X, Y = sp.symbols('X Y')
-    X_o, Y_o = sp.symbols('X_o Y_o') 
     eq = eq.subs({X: X_o, Y: Y_o}) + sp.diff(eq,X).subs({X: X_o, Y: Y_o}) * (X - X_o) + sp.diff(eq,Y).subs({X: X_o, Y: Y_o}) * (Y -Y_o)
     return sp.pprint(eq)
 
@@ -30,5 +29,8 @@ def linearization_system_with_multiple_variables(eq):
 #### how to use 
 X = sp.symbols('X')
 Y = sp.symbols('Y')
+# X_o, Y_o = sp.symbols('X_o Y_o') # can use this or act values
+X_o = 50
+Y_o = 1.23
 eq = (1.23*X)/(Y+2)
-linearization_system_with_multiple_variables(eq)
+linearization_system_with_multiple_variables(eq,X_o,Y_o)
