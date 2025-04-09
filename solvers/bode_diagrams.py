0e0220eb9fbceeb987c40f3509be24ec8cdd3496
X1kiee <oliverl.lohr@gmail.com>
2025-04-09 18:48:29 +0200

    addition of a bode plot with time delay

--- solvers/bode_diagrams.py
+++ solvers/bode_diagrams.py
@@ -17,3 +17,27 @@ num = [40]
 den = [1,2,1]
 plotting_bode_plot(num,den) ####### sample plotting
 
+
+#### bode plot of a system with time delay
+delay = 1
+num, den = ctrl.pade(delay, 8)
+d_fun = ctrl.TransferFunction(num, den)
+# Define transfer function
+h1 = ctrl.TransferFunction([1, 4], [1, 2, 4])
+h2 = h1*d_fun
+# Compute gain margin, phase margin, and crossover frequencies
+gm, pm, wg, wp = ctrl.margin(h2)
+
+
+gm_db = 20 * np.log10(gm)
+print(f"Gain Margin (GM): {gm:.5f}")
+print(f"Gain Margin db = {gm_db:.5f}")
+print(f"Phase Margin (PM): {pm:.5f} degrees")
+print(f"Gain Crossover Frequency (Wg): {wg:.5f} rad/s")
+print(f"Phase Crossover Frequency (Wp): {wp:.5f} rad/s")
+
+# Plot Bode plot
+mag, phase, omega = ctrl.bode_plot(h2, dB=True, Hz=True, margins=True)
+
+# Show plot
+plt.show()
